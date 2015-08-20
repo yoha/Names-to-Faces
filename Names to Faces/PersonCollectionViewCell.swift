d93a600c5d019f46f78c4da1ab04079e61613443
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-08-20 17:14:36 +0700

    Configured the skeleton for instance of UICollectionView

---
+++ Names to Faces/PersonCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCollectionViewCell.swift
+//  Names to Faces
+//
+//  Created by Yohannes Wijaya on 8/20/15.
+//  Copyright © 2015 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - IBOutlet Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
--- Names to Faces/ViewController.swift
+++ Names to Faces/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - IBOutlet properties
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - Required UICollectionViewDataSource Methods
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let eachCell = collectionView.dequeueReusableCellWithReuseIdentifier("PersonCellIdentifier", forIndexPath: indexPath) as! PersonCollectionViewCell
+        return eachCell
+    }
 
 }
 
